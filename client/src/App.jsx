08e34c4a6952d9e31d2ad5dc08b38a797dbb0b58
Pratheek Gowda <gowdapratheek91@gmail.com>
2024-07-03 01:22:29 +0530

    checking router2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/header" element={<Header />} />
-        <Route element={<Err />} />
+        <Route path="/*" element={<Err />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
